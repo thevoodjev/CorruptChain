6430b0e2c9a32c4d3e0d6e5abb7daa48ae706d9a
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2013-09-16 13:15:00 +0100

    refactor: llm-agents

--- src/corruptchain/depends.py
+++ src/corruptchain/depends.py
@@ -8,3 +8,11 @@ ways, and the two are never treated as equal:
 
     inferred   B did not list A, but A's output value appears verbatim inside
                B's output. We infer that B carried A's data forward. Weaker
+               evidence, because a value can coincide, so it is labelled as an
+               inference and never silently promoted to a declared fact.
+
+The honesty rule from the brief lives here: every edge records how it was
+established, and a report can list exactly which edges were inferred. When both
+a declaration and a value match exist for the same pair, the edge is declared,
+because the stronger evidence wins, and the value match is redundant.
+
