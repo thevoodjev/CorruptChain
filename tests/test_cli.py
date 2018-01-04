1c29e24df65ead832c6fa95479321627c4b29d7a
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-04 13:05:00 +0100

    refactor: tidy debugging

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,3 +3,10 @@ import unittest
 from contextlib import redirect_stdout
 from pathlib import Path
 
+from corruptchain import cli
+
+SAMPLES = Path(__file__).resolve().parent.parent / "samples"
+
+
+def run(argv):
+    buf = io.StringIO()
