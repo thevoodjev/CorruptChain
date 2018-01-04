460f0b459948b79e56394ee1abfe2ec51645cfca
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-04 13:17:00 +0100

    refactor: debugging

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,9 @@ SAMPLES = Path(__file__).resolve().parent.parent / "samples"
 
 def run(argv):
     buf = io.StringIO()
+    with redirect_stdout(buf):
+        code = cli.main(argv)
+    return code, buf.getvalue()
+
+
+class TestCli(unittest.TestCase):
