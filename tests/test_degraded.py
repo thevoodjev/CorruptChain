67b2a7d3a818cb76dc8cd1832c1ccfd4c95ab0f7
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-18 18:34:00 +0100

    feat: add python

--- tests/test_degraded.py
+++ tests/test_degraded.py
@@ -5,3 +5,10 @@ from corruptchain.trace import Step
 
 
 def make(kind="tool", status="ok", output="data here",
+         result_count=None, expected_count=None, tool="t"):
+    return Step(
+        id="x", kind=kind, tool=tool, status=status,
+        references=(), output=output,
+        result_count=result_count, expected_count=expected_count,
+    )
+
