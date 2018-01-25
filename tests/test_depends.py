8e7e0f2c3a2234f4d1e0e825990fb7804b4b3e8c
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-25 10:52:00 +0100

    perf: improve debugging

--- tests/test_depends.py
+++ tests/test_depends.py
@@ -4,3 +4,9 @@ from corruptchain import trace
 from corruptchain.depends import DECLARED, INFERRED, build
 
 
+class TestDepends(unittest.TestCase):
+    def test_declared_edge(self):
+        doc = ('{"question": "q", "steps": ['
+               '{"id": "s1", "kind": "tool", "status": "ok",'
+               ' "output": "alpha bravo charlie"},'
+               '{"id": "s2", "kind": "reason", "references": ["s1"],'
