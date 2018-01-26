f2bec2d7588e6cb91d86769dca34d8112a8a641e
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-26 16:37:00 +0100

    feat: cli

--- tests/test_depends.py
+++ tests/test_depends.py
@@ -16,3 +16,10 @@ class TestDepends(unittest.TestCase):
         self.assertEqual(g.edges[0].how, DECLARED)
         self.assertEqual((g.edges[0].source, g.edges[0].consumer), ("s1", "s2"))
 
+    def test_inferred_edge_from_value_match(self):
+        doc = ('{"question": "q", "steps": ['
+               '{"id": "s1", "kind": "tool", "status": "ok",'
+               ' "output": "the special token value"},'
+               '{"id": "s2", "kind": "reason",'
+               ' "output": "carrying the special token value onward"}]}')
+        g = build(trace.parse(doc))
