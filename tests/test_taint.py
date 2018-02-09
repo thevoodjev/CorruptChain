882b56c2063a2d3b9d4921e26141b76751c054eb
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-02-09 16:21:00 +0100

    docs: document taint-analysis

--- tests/test_taint.py
+++ tests/test_taint.py
@@ -6,3 +6,12 @@ from corruptchain.taint import propagate
 
 
 def analyse(doc):
+    t = trace.parse(doc)
+    g = build(t)
+    return t, propagate(t, g)
+
+
+class TestTaint(unittest.TestCase):
+    def test_no_degraded_no_taint(self):
+        doc = ('{"question": "q", "steps": ['
+               '{"id": "s1", "kind": "tool", "status": "ok",'
