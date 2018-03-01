61e8f214f2ea081e2ab15e23a03a20ff4beebe10
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-03-01 11:23:00 +0100

    fix: observability

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -6,3 +6,12 @@ from corruptchain import trace
 class TestTraceParse(unittest.TestCase):
     def test_minimal_valid(self):
         t = trace.parse('{"question": "q", "steps": []}')
+        self.assertEqual(t.question, "q")
+        self.assertEqual(t.steps, ())
+        self.assertIsNone(t.answer)
+
+    def test_parses_step_fields(self):
+        doc = (
+            '{"question": "q", "steps": ['
+            '{"id": "s1", "kind": "tool", "tool": "grep", "status": "ok",'
+            ' "references": [], "output": "hit", "result_count": 1}]}'
