9c1164da919a559f60693751fd22cd9cf6255895
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-03-01 11:28:00 +0100

    test: cover agent-tracing

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -15,3 +15,12 @@ class TestTraceParse(unittest.TestCase):
             '{"question": "q", "steps": ['
             '{"id": "s1", "kind": "tool", "tool": "grep", "status": "ok",'
             ' "references": [], "output": "hit", "result_count": 1}]}'
+        )
+        t = trace.parse(doc)
+        s = t.by_id("s1")
+        self.assertTrue(s.is_tool)
+        self.assertEqual(s.tool, "grep")
+        self.assertEqual(s.result_count, 1)
+
+    def test_answer_is_last_answer_step(self):
+        doc = (
