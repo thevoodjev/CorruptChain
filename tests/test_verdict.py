82237c2e72312a9e1bacb73a9dd9586c32b37890
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-03-31 18:27:00 +0100

    feat: observability

--- tests/test_verdict.py
+++ tests/test_verdict.py
@@ -3,3 +3,10 @@ import unittest
 from corruptchain import trace, verdict
 from corruptchain.depends import build
 from corruptchain.taint import propagate
+
+
+def judge(doc):
+    t = trace.parse(doc)
+    g = build(t)
+    res = propagate(t, g)
+    return verdict.decide(t, res)
